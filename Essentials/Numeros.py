2deda5ccbc7445cbab3382f46c2cdda2c30a9bbb
Jair Garduño <jairgarduno47@gmail.com>
2023-01-18 10:11:23 -0600

    Essentials

---
+++ Essentials/Numeros.py
@@ -0,0 +1,6 @@
+print(0O123) #Numero octal
+print(0x987) #Numero Hexadecimal
+print(3E8) #Esto es igual a 3x10^8
+print(2**3) #Exponenciacion
+print(6//3) #Division entera
+print(12%3) #Operacion de modulo
