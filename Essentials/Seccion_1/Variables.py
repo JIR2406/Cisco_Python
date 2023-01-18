201353bb465962558a3df19a846842757b80b82a
Jair Garduño <jairgarduno47@gmail.com>
2023-01-18 11:00:19 -0600

    Essentials

---
+++ Essentials/Seccion_1/Variables.py
@@ -0,0 +1,19 @@
+a=int(input("Ingresa un numero entero:\t"))
+b=int(input("Ingresa otro numero entero:\t"))
+suma=a+b
+resta=a-b
+multiplicacion=a*b
+exponente=a**b
+division=a/b
+modulo=a%b
+print(suma)
+print(resta)
+print(multiplicacion)
+print(exponente)
+print(division)
+print(modulo)
+x=int(input())
+y=int(input())
+x=x//y
+y=y//x
+print(y)
