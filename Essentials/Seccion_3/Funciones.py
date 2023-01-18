d888d3ae08e5ebe3e0d55cf78a47e943a7bd2824
Jair Garduño <jairgarduno47@gmail.com>
2023-01-18 15:10:05 -0600

    Essentials

---
+++ Essentials/Seccion_3/Funciones.py
@@ -0,0 +1,18 @@
+"""
+Si un fragmento de codigo comienza a aparecer en mas de una ocasion considera la posibilidad de aislarlo
+Si un fragmento de codigo se hace tan extenso para leerlo o entenderlo se hace complicado
+Descomposicion: Compartir el tranajo, compartir la responsabilidad
+Se debe escribir un conjunto ien definido y claro de funciones
+Se debe descomponer el problema para permitir que el producto sea implementado como un conjunto de funciones
+escritas por separado empacadas juntas en diferentes modulos 
+"""
+# def function_name():
+#   cuerpo de la funcion
+
+# Cuando se invoca una funcion Python recuerda el lugar donde esto ocurre
+# El cuerpo de la funcion es entonces ejecutando
+# Al llegar al final de la funcion Python regresa al lugar inmediato despues de donde ocurrio la invocacion
+
+# NO INVOCAR LA FUNCION ANTES DE DEFINIRLA
+
+"""Funciones parametrizadas"""
