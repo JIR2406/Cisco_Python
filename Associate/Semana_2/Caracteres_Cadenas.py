ca6be4aea5ce01f6a87f22c84dd93dedae6a0b5b
Jair Garduño <jairgarduno47@gmail.com>
2023-02-06 14:32:48 -0600

    Associate

---
+++ Associate/Semana_2/Caracteres_Cadenas.py
@@ -0,0 +1,18 @@
+# Caracteres de control: Su propósito es controlar dispositivos de entrada y salida
+# Punto de código: Es un numero que compone un caracter
+"""
+El código ASCII emplea ocho bits para cada signo. Ocho bits significan 256 caracteres diferentes. 
+Los primeros 128 se usan para el alfabeto latino estándar (tanto en mayúsculas como en minúsculas).
+"""
+
+# Unicode: Asigna caracteres únicos (letras, guiones, ideogramas, etc.) a más de un millón de puntos de código
+
+# UCS-4 emplea 32 bits (cuatro bytes) para almacenar cada carácter, y el código es solo el número único de los 
+# puntos de código Unicode.
+
+# UTF-8 emplea tantos bits para cada uno de los puntos de código como realmente necesita para representarlos.
+
+# BOM (Byte Order Mark), una Marca de Orden de Bytes es una combinación especial de bits que anuncia la codificación 
+# utilizada por el contenido de un archivo (por ejemplo, UCS-4 o UTF-B).
+
+pass
